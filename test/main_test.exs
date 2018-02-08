5feadae387303cae7e39c86516b614aee655eda5
Ani Channarasappa <git@ani.dev>
2018-02-08 17:57:05 -0500

    Removed unnecessary properties from test fixtures

--- test/main_test.exs
+++ test/main_test.exs
@@ -3,38 +3,21 @@ defmodule MainTest do
   alias DailyGoals.Main, as: Main
 
   @card_other %{
-    id: "5a6e037b5588952e9ec06eb4",
-    name: "other task",
-    closed: false,
-    idList: "5890a23dead748db3fcbec31",
-    due: "2018-01-29T02:00:00.000Z",
-    dueComplete: false,
-    checklists: []
+    name: "other task"
   }
   @card_parsed_other %{
     card: @card_other,
     date: nil
   }
   @card_old_daily_goal %{
-    id: "5a6e037b5588952e9ec06eb4",
-    name: "Daily Goals - January 29, 2018",
-    closed: false,
-    idList: "5890a23dead748db3fcbec31",
-    due: "2018-01-29T02:00:00.000Z",
-    dueComplete: false,
-    checklists: []
+    name: "Daily Goals - January 29, 2018"
   }
   @card_parsed_old_daily_goal %{
     card: @card_old_daily_goal,
     date: ~D[2018-01-29]
   }
   @card_current_daily_goal %{
-    id: "5a6e037b5588952e9ec06eb4",
     name: "Daily Goals - January 30, 2018",
-    closed: false,
-    idList: "5890a23dead748db3fcbec31",
-    due: "2018-01-29T02:00:00.000Z",
-    dueComplete: false,
     checklists: []
   }
   @card_parsed_current_daily_goal %{
