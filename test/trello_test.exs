e18355e4ed2f19deb5168f890778ce511d4dcc27
Ani Channarasappa <git@ani.dev>
2018-02-08 21:37:02 -0500

    Added check for test environment variables

--- test/trello_test.exs
+++ test/trello_test.exs
@@ -2,9 +2,27 @@ defmodule TrelloTest do
   use ExUnit.Case
   alias DailyGoals.Trello, as: Trello
 
-  @tag :io
-  test "get_cards" do
+  @required_env [
+    :trello_api_key,
+    :trello_oauth_token,
+    :trello_board_id
+  ]
+
+  setup_all do
     config = Application.get_all_env(:app)
+
+    @required_env
+    |> MapSet.new()
+    |> MapSet.subset?(config |> Keyword.keys() |> MapSet.new())
+    |> IO.inspect()
+    |> case do
+      false -> raise "Environment variables for testing not set!"
+      _ -> config
+    end
+  end
+
+  @tag :io
+  test "get_cards", config do
     expected = ["checklists", "closed", "due", "dueComplete", "id", "idList", "name"]
 
     assert Trello.get_cards(
@@ -18,5 +36,6 @@ defmodule TrelloTest do
 
   @tag :io
   test "create_card" do
+    assert true == true
   end
 end
