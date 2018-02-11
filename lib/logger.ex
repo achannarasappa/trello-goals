121c2619cb57ef3b8f3c573aeb60daab88b5d652
Ani Channarasappa <git@ani.dev>
2018-02-11 15:36:04 -0500

    Added newline prefix to log lines

--- lib/logger.ex
+++ lib/logger.ex
@@ -2,14 +2,15 @@ defmodule DailyGoals.Logger do
   import Poison
 
   def format(level, message, _, metadata) do
-    Poison.encode!(%{
-      time: DateTime.utc_now() |> DateTime.to_iso8601(),
-      level: level,
-      msg: message,
-      meta: %{
-        function: metadata[:function],
-        module: metadata[:module]
-      }
-    })
+    "\n" <>
+      Poison.encode!(%{
+        time: DateTime.utc_now() |> DateTime.to_iso8601(),
+        level: level,
+        msg: message,
+        meta: %{
+          function: metadata[:function],
+          module: metadata[:module]
+        }
+      })
   end
 end
